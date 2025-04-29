eefabab8d3e128957c622b096307655d62bafd20
Arthur Chardon <arthur.chardon74@gmail.com>
2025-04-29 10:36:39 +0200

    feat: agribalise-ref as json instead of tsv

--- ecobalyse-food-front/src/context/ProductAttributesContext.tsx
+++ ecobalyse-food-front/src/context/ProductAttributesContext.tsx
@@ -25,32 +25,37 @@ export function ProductAttributesProvider({
 
   useEffect(() => {
     const fetchCategories = async () => {
-      const fileContent = await fetch("agribalise-ref.tsv").then((res) => {
-        return res.text();
+      const fileContent = await fetch("agribalise-ref.json").then((res) => {
+        return res.json();
       });
       try {
-        console.log("fileContent", fileContent);
         const newCategories: ProductCategory[] = [];
         const trackLabels: string[] = [];
         const trackDuplicates: string[] = [];
-        const arrayValues = fileContent
-          .split("\r\n")
-          .slice(1)
-          .map((row) => row.split("\t"));
-        arrayValues.forEach((row, index) => {
-          if (trackLabels.includes(row[0])) {
-            trackDuplicates.push(row[0]);
+        const trackNoValue: string[] = [];
+        const arrayValues = fileContent.agribaliseRef as {
+          name: string;
+          agbScore: number | null;
+        }[];
+        arrayValues.forEach((productCategory, index) => {
+          if (trackLabels.includes(productCategory.name)) {
+            trackDuplicates.push(productCategory.name);
             return;
           }
-          trackLabels.push(row[0]);
+          if (productCategory.agbScore === null) {
+            trackNoValue.push(productCategory.name);
+            return;
+          }
+          trackLabels.push(productCategory.name);
           newCategories.push({
             id: index,
-            name: row[0],
-            agbScore: parseFloat(row[1]),
+            name: productCategory.name,
+            agbScore: productCategory.agbScore,
           });
         });
         setCategories(newCategories);
-        console.log("Duplicates", trackDuplicates);
+        console.log("[AGB Data] Duplicates", [trackDuplicates]);
+        console.log("[AGB Data] No values", [trackNoValue]);
       } catch (error) {
         console.error("Error fetching categories:", error);
       }
@@ -60,7 +65,6 @@ export function ProductAttributesProvider({
         return res.json();
       });
       try {
-        console.log("fileContent", fileContent);
         const newLabels: ProductLabel[] = fileContent.labels.map(
           (label: any) => {
             return {
